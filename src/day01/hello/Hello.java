df073ff06849f27cbe2400884d86c18442e4d944
seonggeunK <tjdrmsdl357@gmail.com>
2022-11-14 20:01:18 +0900

    첫커밋

---
+++ src/day01/hello/Hello.java
@@ -0,0 +1,11 @@
+package day01.hello;
+
+public class Hello {
+	//실행 단축키 F11 컨트롤 + 스페이스 = 자동완성
+	public static void main(String[] args) {
+		System.out.println("Hello, World");
+	}
+
+	}
+
+
---
+++ src/day02/test/MainClass01.java
@@ -0,0 +1,9 @@
+package day02.test;
+
+public class MainClass01 {
+	// 프로그램 시작이 되는 메인 메소드
+	public static void main(String[] args) {
+		
+	}	
+
+}
